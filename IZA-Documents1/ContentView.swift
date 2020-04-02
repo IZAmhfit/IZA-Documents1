701eb11318b4d561d5987c287dc718e92a699f18
Martin Hrubý <hrubym@fit.vutbr.cz>
2020-04-02 10:35:31 +0200

    Initial Commit

---
+++ IZA-Documents1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IZA-Documents1
+//
+//  Created by Martin Hruby on 02/04/2020.
+//  Copyright © 2020 Martin Hruby FIT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
